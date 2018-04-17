83cc9c08e9933ce72d1a381eb8e44c4f667726ad
panhaifeng <613554008@qq.com>
2018-04-17 19:41:45 +0800

    调试1

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,20 +3,26 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $signature = $_GET["signature"];//从用户端获取签名赋予变量signature
-		$timestamp = $_GET["timestamp"];//从用户端获取时间戳赋予变量timestamp
-		$nonce = $_GET["nonce"];    //从用户端获取随机数赋予变量nonce
 
-		$token ='YoonPer';//将常量token赋予变量token
-		$tmpArr = array($token, $timestamp, $nonce);//简历数组变量tmpArr
-		sort($tmpArr, SORT_STRING);//新建排序
-		$tmpStr = implode( $tmpArr );//字典排序
-		$tmpStr = sha1( $tmpStr );//shal加密
-		//tmpStr与signature值相同，返回真，否则返回假
-		if( $tmpStr == $signature ){
-		echo $_GET["echostr"];
-		}else{
-		return false;
-		}
+	       /* $signature = $_GET["signature"];//从用户端获取签名赋予变量signature
+			$timestamp = $_GET["timestamp"];//从用户端获取时间戳赋予变量timestamp
+			$nonce = $_GET["nonce"];    //从用户端获取随机数赋予变量nonce
+
+			$token ='YoonPer';//将常量token赋予变量token
+			$tmpArr = array($token, $timestamp, $nonce);//简历数组变量tmpArr
+			sort($tmpArr, SORT_STRING);//新建排序
+			$tmpStr = implode( $tmpArr );//字典排序
+			$tmpStr = sha1( $tmpStr );//shal加密
+			//tmpStr与signature值相同，返回真，否则返回假
+			if( $tmpStr == $signature ){
+			echo $_GET["echostr"];
+			}else{
+			return false;
+			}*/
+         $strP = file_get_contents("php://input");
+         $postObj = simplexml_load_string($strP, 'SimpleXMLElement', LIBXML_NOCDATA);
+         
 	}
+
+
 }
