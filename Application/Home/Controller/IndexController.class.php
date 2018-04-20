49b14449e81233304213dd08739546aa51665eec
panhaifeng <613554008@qq.com>
2018-04-21 00:40:11 +0800

    嗯嗯1

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+$wechatObj = new IndexController();
+$wechatObj->responseMsg();
 class IndexController extends Controller {
     public function index(){
 
@@ -23,6 +25,42 @@ class IndexController extends Controller {
          $postObj = simplexml_load_string($strP, 'SimpleXMLElement', LIBXML_NOCDATA);
          dump2file(1);*/
 	}
+	public function responseMsg()
+ {
+  //get post data, May be due to the different environments
+  $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+ 
+  //extract post data
+  if (!empty($postStr)){
+     
+    $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+    $fromUsername = $postObj->FromUserName;
+    $toUsername = $postObj->ToUserName;
+    $keyword = trim($postObj->Content);
+    $time = time();
+    $textTpl = "<xml>
+       <ToUserName><![CDATA[%s]]></ToUserName>
+       <FromUserName><![CDATA[%s]]></FromUserName>
+       <CreateTime>%s</CreateTime>
+       <MsgType><![CDATA[%s]]></MsgType>
+       <Content><![CDATA[%s]]></Content>
+       <FuncFlag>0</FuncFlag>
+       </xml>";    
+    if(!empty( $keyword ))
+    {
+     $msgType = "text";
+     $contentStr = "Welcome to wechat world!";
+     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+     echo $resultStr;
+    }else{
+     echo "Input something...";
+    }
+ 
+  }else {
+   echo "";
+   exit;
+  }
+ }
 
 
 }
