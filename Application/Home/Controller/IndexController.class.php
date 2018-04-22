426c3af4a2f4b2993991251b86286672c3baf19b
panhaifeng <613554008@qq.com>
2018-04-22 21:25:50 +0800

    和hi

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,7 +6,7 @@ $wechatObj->responseMsg();
 class IndexController extends Controller {
     public function index(){
 
-	        $signature = $_GET["signature"];//从用户端获取签名赋予变量signature
+	        $signature = $_GET["signature"];//从用户端获取签名赋予变量signature11
 			$timestamp = $_GET["timestamp"];//从用户端获取时间戳赋予变量timestamp
 			$nonce = $_GET["nonce"];    //从用户端获取随机数赋予变量nonce
 
