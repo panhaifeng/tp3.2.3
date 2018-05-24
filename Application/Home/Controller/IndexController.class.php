1816cc73c8a92badf42c593869b1d655487088e9
panhaifeng <613554008@qq.com>
2018-05-24 15:53:42 +0800

    微信token配置

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,82 +2,134 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-   /* public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
-    }*/
-    public function __construct(){
-
+    public function index()
+    {
+        $this->show('This is for Wechat','utf-8');
     }
-    public function index(){
-        //获得参数 signature nonce token timestamp echostr
-        $nonce     = $_GET['nonce'];
-        $token     = 'imooc';
-        $timestamp = $_GET['timestamp'];
-        $echostr   = $_GET['echostr'];
-        $signature = $_GET['signature'];
-        //形成数组，然后按字典序排序
-        $array = array();
-        $array = array($nonce, $timestamp, $token);
-        sort($array);
-        //拼接成字符串,sha1加密 ，然后与signature进行校验
-        $str = sha1( implode( $array ) );
-        if( $str  == $signature && $echostr ){
-            //第一次接入weixin api接口的时候
-            echo  $echostr;
-            exit;
+    //用户首次配置开发环境
+    public function echoStr()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce     = $_GET["nonce"];
+        $echostr   = $_GET['echostr'];      
+    $token     = 'skye';
+    $tmpArr    = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr    = implode( $tmpArr );
+    $tmpStr    = sha1( $tmpStr );
+    if( $tmpStr == $signature && $echostr)
+    {
+        echo $echostr;
+    }else{
+        $this->reposeMsg();
+    }
+    }
+
+    //回复消息
+    public function reposeMsg()
+    {
+        //1.接受数据
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];  //接受xml数据
+        //2.处理消息类型,推送消息
+    $postObj = simplexml_load_string( $postArr );   //将xml数据转化为对象
+    if( strtolower( $postObj->MsgType ) == 'event')
+    {   
+        //关注公众号事件
+        if( strtolower( $postObj->Event ) == 'subscribe' )
+        {
+            $toUser    =  $postObj->FromUserName;
+            $fromUser  =  $postObj->ToUserName;
+            $time      =  time();
+            $msgType   =  'text';
+            $content   =  '你终于来啦,等你等的好辛苦啊!可尝试输入关键字:教程,Tel,wechat,1等000';
+            $template  =  "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    </xml>";
+            echo sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+        }
+    }
+
+    //回复文本信息
+    if( strtolower( $postObj->MsgType ) == 'text' && trim($postObj->Content)=='wechat')
+    {
+        $toUser = $postObj->FromUserName;
+        $fromUser = $postObj->ToUserName;
+        $arr = array(
+            array(
+                'title'=>'test',
+                'description'=>"just so so...",
+                'picUrl'=>'http://www.acting-man.com/blog/media/2014/11/secret-.jpg',
+                'url'=>'http://www.imooc.com',
+            ),
+            array(
+                'title'=>'hao123',
+                'description'=>"hao123 is very cool",
+                'picUrl'=>'https://www.baidu.com/img/bdlogo.png',
+                'url'=>'http://www.hao123.com',
+            ),
+            array(
+                'title'=>'qq',
+                'description'=>"qq is very cool",
+                'picUrl'=>'http://www.imooc.com/static/img/common/logo.png',
+                'url'=>'http://www.qq.com',
+            ),
+        );
+        $template = "<xml>
+                 <ToUserName><![CDATA[%s]]></ToUserName>
+                 <FromUserName><![CDATA[%s]]></FromUserName>
+                 <CreateTime>%s</CreateTime>
+                 <MsgType><![CDATA[%s]]></MsgType>
+                 <ArticleCount>".count($arr)."</ArticleCount>
+                 <Articles>";
+        foreach($arr as $k=>$v){
+            $template .="<item>
+                    <Title><![CDATA[".$v['title']."]]></Title> 
+                    <Description><![CDATA[".$v['description']."]]></Description>
+                    <PicUrl><![CDATA[".$v['picUrl']."]]></PicUrl>
+                    <Url><![CDATA[".$v['url']."]]></Url>
+                    </item>";
+        }
+            
+        $template .="</Articles>
+                 </xml> ";
+        echo sprintf($template, $toUser, $fromUser, time(), 'news');
+        //注意：进行多图文发送时，子图文个数不能超过10个
         }else{
-            $this->reponseMsg();
-        }
-    }
-
-    public function  show(){
-        echo 'imooc';
-    }
-
-    public function reponseMsg(){
-        //1.获取到微信推送过来post数据（xml格式）
-        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
-        //2.处理消息类型，并设置回复类型和内容
-        /*<xml>
-<ToUserName><![CDATA[toUser]]></ToUserName>
-<FromUserName><![CDATA[FromUser]]></FromUserName>
-<CreateTime>123456789</CreateTime>
-<MsgType><![CDATA[event]]></MsgType>
-<Event><![CDATA[subscribe]]></Event>
-</xml>*/
-        $postObj = simplexml_load_string( $postArr );
-        //$postObj->ToUserName = '';
-        //$postObj->FromUserName = '';
-        //$postObj->CreateTime = '';
-        //$postObj->MsgType = '';
-        //$postObj->Event = '';
-        // gh_e79a177814ed
-        //判断该数据包是否是订阅的事件推送
-        if( strtolower( $postObj->MsgType) == 'event'){
-            //如果是关注 subscribe 事件
-            if( strtolower($postObj->Event == 'subscribe') ){
-                //回复用户消息(纯文本格式)
-                $toUser   = $postObj->FromUserName;
-                $fromUser = $postObj->ToUserName;
-                $time     = time();
-                $msgType  =  'text';
-                $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
-                $template = "<xml>
-                            <ToUserName><![CDATA[%s]]></ToUserName>
-                            <FromUserName><![CDATA[%s]]></FromUserName>
-                            <CreateTime>%s</CreateTime>
-                            <MsgType><![CDATA[%s]]></MsgType>
-                            <Content><![CDATA[%s]]></Content>
-                            </xml>";
-                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
-                echo $info;
-                /*<xml>
-                <ToUserName><![CDATA[toUser]]></ToUserName>
-                <FromUserName><![CDATA[fromUser]]></FromUserName>
-                <CreateTime>12345678</CreateTime>
-                <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA[你好]]></Content>
-                </xml>*/
+            switch( trim( $postObj->Content ) )
+            {
+                case 1:
+                    $content = '你输入了个数字1';
+                    break;
+                case Tel:
+                    $content = '12345678901';
+                    break;
+                case '教程':
+                    $content = "<a href='www.imooc.com'>慕课网</a>";
+                    break;
+                case '博客':
+                    $content = "<a href='blog.abc.com'>测试微信</a>";
+                    break;
+                default:
+                    $content = '升级打造中...';
+                    break;
             }
+            $toUser     =  $postObj->FromUserName;
+            $fromUser   =  $postObj->ToUserName;
+            $time       =  time();
+            $msgType    =  'text';
+            $template   =  "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                        </xml>";
+            echo sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
         }
-}}
+    }
+}
